d5bf327ed7d58d65f5f9464bc28cfac30b29981c
Lucas <lucas@agtlucas.com>
2015-11-28 22:27:27 -0200

    Update store

--- components/Footer.js
+++ components/Footer.js
@@ -25,7 +25,7 @@ class Footer extends Component {
     const { filter: selectedFilter, onShow } = this.props;
 
     return(
-      <a className={classnames({ selected: filter === selectedFilter })} style={{ cursor: pointer }} onClick={() => onShow(filter)}>
+      <a className={classnames({ selected: filter === selectedFilter })} style={{ cursor: 'pointer' }} onClick={() => onShow(filter)}>
         {title}
       </a>
     )
